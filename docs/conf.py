b5a1652f539d7dfed4a73a7ef65d611447134932
FeignClaims <www4112379@163.com>
2024-01-29 23:45:55 +0800

    修复到源文件的跳转链接

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ html_show_sourcelink = False
 
 html_theme = 'sphinx_book_theme'
 html_theme_options = {
-    'repository_url': 'https://github.com/FeignClaims/question_board',
+    'repository_url': 'https://github.com/FeignClaims/change_major_exam',
     'path_to_docs': 'docs/source/',
     'use_source_button': True,
     'use_edit_page_button': True,
