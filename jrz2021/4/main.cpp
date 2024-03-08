d39d60f4679d6e0d71c4d9df23dc834b74cc6697
FeignClaims <www4112379@163.com>
2024-03-08 20:43:36 +0800

    退休

--- jrz2021/4/main.cpp
+++
@@ -1,8 +0,0 @@
-int main() {
-  FinalTest item1("C++ Test", Date(2014, 6, 2));
-  item1.print();
-  FinalTest item2("Java");
-  item2.print();
-  item2.setDue(Date(2014, 6, 10));
-  item2.print();
-}
